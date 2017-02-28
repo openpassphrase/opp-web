7deebfabd293b76bd1b93e96aaa9cd180d424e42
DmitryEfimenko <dmitrief@gmail.com>
2017-02-28 01:38:14 -0700

    fixes #17

--- src/app/reducers/items.ts
+++ src/app/reducers/items.ts
@@ -24,9 +24,11 @@ export function reducer(state = initialState, action: item.Actions): State {
       items: [...state.items, action.payload]
     };
   } else if (action instanceof item.AddItemSuccessAction) {
+    console.log(state.items);
+    console.log('s', action.payload);
     return {
       loading: false,
-      items: state.items.map(x => x.id === undefined && x.name === action.payload.name
+      items: state.items.map(x => x.id === null && x.name === action.payload.name
         ? Object.assign({}, x, { id: action.payload.id })
         : x)
     };
