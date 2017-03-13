9241d8d1957a49f5f9beea9fdda721ed0fb2fdd7
Alexander Bashmakov <abashmak@yahoo.com>
2017-03-12 18:05:57 -0700

    Added month/day padding in date generation

--- make_version.js
+++ make_version.js
@@ -1,7 +1,7 @@
 var package = require('./package.json');
 var fs = require('fs');
 var date = new Date();
-var today = [date.getFullYear(), date.getMonth() + 1, date.getDate()].join('-');
+today = date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
 version_string = '{"version":"' + package.version + '-' + today + '"}';
 fs.writeFile('src/assets/version.json', version_string, function(err) {
   if(err) {
