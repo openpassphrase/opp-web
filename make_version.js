47d02713cf0642dd5fed1c9dd5056a7c6c606ceb
Alexander Bashmakov <abashmak@yahoo.com>
2017-03-12 17:54:37 -0700

    Update application title and version generation.
    Changed title tag from OppWeb to OpenPassPharse.
    Added auto-generation and appending of date for the version.

--- make_version.js
+++ make_version.js
@@ -1,6 +1,8 @@
 var package = require('./package.json');
 var fs = require('fs');
-version_string = '{"version":"' + package.version + '"}';
+var date = new Date();
+var today = [date.getFullYear(), date.getMonth() + 1, date.getDate()].join('-');
+version_string = '{"version":"' + package.version + '-' + today + '"}';
 fs.writeFile('src/assets/version.json', version_string, function(err) {
   if(err) {
     return console.log(err);
