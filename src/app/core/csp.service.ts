5291bab4945b109e7444be74a68b21a667601f9f
DmitryEfimenko <dmitrief@gmail.com>
2018-09-08 21:05:06 -0700

    more csp

--- src/app/core/csp.service.ts
+++ src/app/core/csp.service.ts
@@ -2,7 +2,14 @@ import { DOCUMENT } from '@angular/common';
 import { Inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 
-
+/**
+ * Used to enforce CSP policy. For more details about CSP, see
+ * https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+ * and https://developers.google.com/web/fundamentals/security/csp/
+ * Evaluate your CSP here: https://csp-evaluator.withgoogle.com/
+ * Remove 'unsafe-inline' in style-src once Angular issue is resolved:
+ * https://github.com/angular/angular/issues/6361
+ */
 @Injectable()
 export class CspService {
   constructor(
@@ -13,13 +20,16 @@ export class CspService {
     if (environment.name !== 'dev') {
       const meta: HTMLMetaElement = this.doc.createElement('meta');
       meta.setAttribute('http-equiv', 'Content-Security-Policy');
-      meta.setAttribute(
-        'content',
-        `default-src 'self';
-         child-src 'none';
-         font-src 'self' data:;
-         style-src 'unsafe-inline';
-         script-src 'self' 'unsafe-eval'`
+      meta.setAttribute('content', `
+        default-src 'self';
+        font-src 'self' data:;
+        style-src 'self' 'unsafe-inline';
+        script-src 'self';
+        worker-src 'self';
+        object-src 'none';
+        form-action 'none';
+        frame-src 'none';
+        `
       );
       this.doc.head.appendChild(meta);
     }
