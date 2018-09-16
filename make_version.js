dd9b9f051334f3cab61bae41f1457c4f43531dfb
Alexander Bashmakov <abashmak@yahoo.com>
2018-09-15 17:23:59 -0700

    Added icons for iOS. (#61)
    Also re-factored the make_version.js script to be more modular.

--- make_version.js
+++ make_version.js
@@ -1,8 +1,12 @@
 var package = require('./package.json');
 var fs = require('fs');
 var date = new Date();
-var today = date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2)  + '-' + ('0' + date.getHours()).slice(-2) + '-' + ('0' + date.getMinutes()).slice(-2);
-var version_string = '{"version":"' + package.version + '-' + today + '"}';
+var month = ('0' + (date.getMonth() + 1)).slice(-2);
+var day = ('0' + date.getDate()).slice(-2);
+var hour = ('0' + date.getHours()).slice(-2);
+var minute = ('0' + date.getMinutes()).slice(-2);
+var version = date.getFullYear() + '-' + month + '-' + day  + '-' + hour + '-' + minute;
+var version_string = '{"version":"' + package.version + '-' + version + '"}';
 fs.writeFile('src/assets/version.json', version_string, function(err) {
   if(err) {
     return console.log(err);
