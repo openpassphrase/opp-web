071d7140008fc07f3ec16e42a40c35e51989b1bb
Alexander Bashmakov <abashmak@yahoo.com>
2018-09-19 10:17:35 -0700

    Update app icons. (#66)

---
+++ src/assets/icons/generate_icon_sizes.sh
@@ -0,0 +1,13 @@
+convert -geometry x76 icon-512x512.png apple-icon-76x76.png
+convert -geometry x120 icon-512x512.png apple-icon-120x120.png
+convert -geometry x152 icon-512x512.png apple-icon-152x152.png
+convert -geometry x167 icon-512x512.png apple-icon-167x167.png
+convert -geometry x180 icon-512x512.png apple-icon-180x180.png
+
+convert -geometry x96 icon-512x512.png icon-96x96.png
+convert -geometry x72 icon-512x512.png icon-72x72.png
+convert -geometry x128 icon-512x512.png icon-128x128.png
+convert -geometry x144 icon-512x512.png icon-144x144.png
+convert -geometry x152 icon-512x512.png icon-152x152.png
+convert -geometry x192 icon-512x512.png icon-192x192.png
+convert -geometry x384 icon-512x512.png icon-384x384.png
