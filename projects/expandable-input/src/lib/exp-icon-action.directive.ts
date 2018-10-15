8e5f5d4d0ffb99aa7e24335b831975006039c5b6
Dmitry A. Efimenko <dmitrief@gmail.com>
2018-10-14 18:57:08 -0700

    use @ng-espandable-input (#67)

--- projects/expandable-input/src/lib/exp-icon-action.directive.ts
+++
@@ -1,7 +0,0 @@
-import { Directive } from '@angular/core';
-
-@Directive({
-  selector: '[cdkExpIconAction]'
-})
-export class CdkExpIconActionDirective {
-}
--- projects/expandable-input/src/public_api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of expandable-input
- */
-
-export * from './lib/expandable-input.component';
-export * from './lib/expandable-input.module';
-export * from './lib/inputs-manager.service';
-
